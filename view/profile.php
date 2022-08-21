fce1735a68af961d6ffcff959d77d7f8a6895136
Saurabh Saraswat <saurabh45215@gmail.com>
2022-08-21 21:06:21 +0530

    Update profile.php

--- view/profile.php
+++ view/profile.php
@@ -8,11 +8,18 @@ if (! empty($_SESSION["userId"])) {
     $memberResult = $member->getMemberById($_SESSION["userId"]);
     if(!empty($memberResult[0]["display_name"])) {
         $displayname = ucwords($memberResult[0]["display_name"]);
+        $username = ucwords($memberResult[0]["user_name"]);
+        $email = ucwords($memberResult[0]["email"]);
         $profile_pic = ucwords($memberResult[0]["img"]);
     } else {
         $displayname = $memberResult[0]["display_name"];
+        $username = $memberResult[0]["user_name"];
+        $email = $memberResult[0]["email"];
         $profile_pic = $memberResult[0]["img"];
     }
 }
 echo "<img src='$profile_pic'  width='70%'>";
+echo $displayname;
+echo $username;
+echo $email;
 ?>
