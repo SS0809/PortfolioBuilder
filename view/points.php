46483066428b996266edfa98e7e820467a4d9d83
Saurabh Saraswat <saurabh45215@gmail.com>
2022-08-30 22:01:25 +0530

    Update points.php

--- view/points.php
+++ view/points.php
@@ -40,6 +40,7 @@ if($suggest != "")//not empty
 <!DOCTYPE html>
 <html lang="en">
   <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8" />
       <!--BOOTSTRAP-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
