8cdd2d41577aa2d803966d57be4fad2897df24d5
SS0809 <saurabh45215@gmail.com>
2022-08-30 20:32:59 +0530

    Update points.php

--- view/points.php
+++ view/points.php
@@ -41,6 +41,11 @@ if($suggest != "")//not empty
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+      <!--BOOTSTRAP-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!--BOOTSTRAP-->
   </head>
 <body>
 <h2>EARN POINTS</h2>
