f84d200e0c35a4c850b0726ceaaa9c42d112a275
SS0809 <saurabh45215@gmail.com>
2022-09-01 19:30:18 +0530

    0

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if(!empty($_SESSION["userId"])) {        
+if(!empty($_SESSION["userId"])) {          
+    $new="12";
     require_once './view/dashboardnew.php';
 } else {   
     require_once './view/login-form.php';
