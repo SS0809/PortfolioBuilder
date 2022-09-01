bd754214b7da21df929dedfca67f72516b7550cc
SS0809 <saurabh45215@gmail.com>
2022-09-01 14:29:16 +0530

    Update movies.php

--- view/movies.php
+++ view/movies.php
@@ -36,7 +36,7 @@ if (! empty($_SESSION["userId"])) {
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">SUBXSUB</a>
+      <a class="navbar-brand" href="/index.php">SUBXSUB</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
