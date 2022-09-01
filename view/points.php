032a5d6bcd880438ad0004a6c4ec339b8b2d98b2
SS0809 <saurabh45215@gmail.com>
2022-09-01 10:27:17 +0530

    style

--- view/points.php
+++ view/points.php
@@ -1,2 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once("a.html") ?>
+</head>
+<body>
+<script type="text/javascript">
+        points.setAttribute("href", "points.php");
+        points.setAttribute("class", "nav-link active");
+        blogs.setAttribute("href", "../blog/index.html");
+        movies.setAttribute("href", "movies.php");
+        yt.setAttribute("href", "../yt/index.html");
+        upload.setAttribute("href", "./upload2.php");
+        logout.setAttribute("href", "/logout.php");
+        home.setAttribute("href", "../index.php");
+        home.setAttribute("class", "nav-link");
+</script>
+
+
+
+
+
+
 points2.php?c0123l123i123c546k123c8l123i45812c3458k123=0
 points1.php?c0123l123i123c546k123c8l123i45812c3458k123=0
+<?php include_once("footer.html") ?>
+</body>
+</html>
