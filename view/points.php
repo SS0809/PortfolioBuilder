0c0cbbc0375f06b4f69b23c4d37e8401ac1043a4
SS0809 <saurabh45215@gmail.com>
2022-09-05 15:43:30 +0530

    Update points.php

--- view/points.php
+++ view/points.php
@@ -88,7 +88,7 @@ if($suggest != "")//not empty
 <br><br><br /><br />
     <div style="text-align: center;">
 START CLICKING ADS ... AND EARN POINTS IN EVERY 10 POINTS YOU ARE NOTIFIED<br>
-    <button style="background: rgb(54, 182, 0); border-radius: 3px; border: transparent; box-shadow: rgb(68, 68, 68) 2px 2px 2px; color: white; font-family: Roboto, sans-serif; font-size: 20px; font-weight: 600; padding: 13px; text-decoration: none;"><a href="/view/points1.php?c0123l123i123c546k123c8l123i45812c3458k123=0">SERVER 1</a></button>
+    <button style="background: rgb(54, 182, 0); border-radius: 3px; border: transparent; box-shadow: rgb(68, 68, 68) 2px 2px 2px; color: white; font-family: Roboto, sans-serif; font-size: 20px; font-weight: 600; padding: 13px; text-decoration: none;" disabled><a href="/view/points1.php?c0123l123i123c546k123c8l123i45812c3458k123=0">SERVER 1</a></button>
 
 </div><br>
     <div style="text-align: center;">
