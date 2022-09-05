48ca672d46ec5f2a3f7205adb00b258228bfda51
SS0809 <saurabh45215@gmail.com>
2022-09-05 10:51:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if(!empty($_SESSION["userId"])) {          
+if(!empty($_SESSION["userId"])) { 
+           $new="gfhfhf";         
     require_once './view/dashboardnew.php';
 } else {   
     require_once './view/login-form.php';
