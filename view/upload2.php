2cb949ba83783573bb43d300e05f2647f2cbac94
SS0809 <saurabh45215@gmail.com>
2022-09-06 14:29:45 +0530

    Update upload2.php

--- view/upload2.php
+++ view/upload2.php
@@ -25,6 +25,8 @@ $result = mysqli_query($db, $query);
 ?>
 <html lang="en">
 <head>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 </head>
 <script>
