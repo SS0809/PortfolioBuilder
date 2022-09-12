f0fd3415cb438018911c7d8b1a80fe53262d1c1b
Saurabh Saraswat <saurabh45215@gmail.com>
2022-09-12 16:16:08 +0530

    Create preload.php

---
+++ view/preload.php
@@ -0,0 +1,50 @@
+<!DOCTYPE>  
+<html lang='en'>
+<head>
+    <title> Loader example </title>
+    <style>
+        .loading-gif {
+            max-width: 50px;
+        }
+
+        .pre-loader {
+            position: fixed;
+            z-index: 100; /** make sure this is the highest value compared to all other divs **/
+            top: 0;
+            left: 0;
+            background: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100%;
+        }
+
+        .pre-loader.hidden {
+            animation: fadeOut 5s; /** change to 1s */
+            animation-fill-mode: forwards;
+        }
+
+        @keyframes fadeOut {
+            100% {
+                opacity: 0;
+                visibility: hidden;
+            }
+        }
+    </style>
+</head>
+<body>
+<!--- Your content goes here -->
+<p>Page content</p>
+<div class='pre-loader'>
+    <img class='loading-gif' alt='loading' src="https://c.tenor.com/UnFx-k_lSckAAAAM/amalie-steiness.gif"/>
+</div>
+
+<!-- js to hide preloader loading is done -->
+<script type='text/javascript'>
+    window.addEventListener('load', function () {
+        document.querySelector('.pre-loader').className += ' hidden';
+    });
+</script>
+</body>
+</html>
