554bcae2331b11a680ccad30eb689c7c674b801b
Saurabh Saraswat <saurabh45215@gmail.com>
2022-09-12 16:41:58 +0530

    Update dashboardnew.php

--- view/dashboardnew.php
+++ view/dashboardnew.php
@@ -124,7 +124,7 @@ color:orange;
 <br><br>
 <div>
         <div>
-            <img src='<?php echo $profile_pic; ?>' style="max-width:25%;">
+            <img src='<?php echo $profile_pic; ?>' style="max-width:15%;">
         </div> 
     <div style='margin-left:60px;'>
     <div style="font-size:1em;float:left;">
