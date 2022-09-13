623401e90b52722f9eaaf75d99f69e7cfa8c8011
Saurabh Saraswat <saurabh45215@gmail.com>
2022-09-13 14:03:20 +0530

    Update new.php

--- view/new.php
+++ view/new.php
@@ -68,18 +68,18 @@ $di= '<div id="container-'.$temp_link.'"></div>';
 </head>
 <body>
   <br><br><br>
-  <h1>PAGE total 30</h1>
+  <h1>PAGE total 46</h1>
   <!--c0123l123i123c546k123c8l123i45812c3458k123-->
   <h2>TOKEN <?php echo $recieve; ?></h2>
   <h1>Points ::<a id="clicks">000</a></h1>
   <div class="float-end"><a href="/index.php" class="btn btn-info me-1">Profile</a></div>
-  <form id="myForm" action="/view/new.php?page=<?php   if($recieve==30){echo "1";}else{echo $recieve+1;}?>" method="post">
+  <form id="myForm" action="/view/new.php?page=<?php   if($recieve==46){echo "1";}else{echo $recieve+1;}?>" method="post">
     <input type="hidden" id="suggest" name="suggest" value="NO">
   </form>
   <br>
 <?php echo $sc;  echo $di; include "searchparam.php";?>
     function create() {
-      window.location = '/view/new.php?page=' + <?php if($recieve==30){echo "1";}else{echo $recieve+1;}?>;
+      window.location = '/view/new.php?page=' + <?php if($recieve==46){echo "1";}else{echo $recieve+1;}?>;
       console.log("2 success");
     }
  <?php include "redirector.php";?>
