acfdffba4279037729d9ba14444c291491dce1b4
Saurabh Saraswat <saurabh45215@gmail.com>
2022-09-17 21:16:59 +0530

    Update super.php

--- view/super.php
+++ view/super.php
@@ -144,7 +144,7 @@ console.log(str);
             clearInterval(timeinterval);
             create();
           }
-        }, 1500);
+        }, 1750);
     }
     su = document.getElementById('clicks');
     su.textContent = str;
