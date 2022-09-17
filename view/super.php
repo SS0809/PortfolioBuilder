62f964d76f2428841726006f718766b0ed74ab5f
SS0809 <saurabh45215@gmail.com>
2022-09-17 09:26:38 +0530

    up

--- view/super.php
+++ view/super.php
@@ -144,7 +144,7 @@ console.log(str);
             clearInterval(timeinterval);
             create();
           }
-        }, 1000);
+        }, 1500);
     }
     su = document.getElementById('clicks');
     su.textContent = str;
