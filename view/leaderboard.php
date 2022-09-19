1fc978305b57094be092567722eb7702c6967272
SS0809 <saurabh45215@gmail.com>
2022-09-19 12:38:01 +0530

    Update leaderboard.php

--- view/leaderboard.php
+++ view/leaderboard.php
@@ -29,6 +29,30 @@ namespace Php;
 <canvas id="chart1"  style="width:50%;max-width:750px"></canvas>
 </div>
 <br><br>
+<!---ADS--->
+<script type="text/javascript">
+  atOptions = {
+    'key' : '89d61dc2a52f9413360c65d32714d577',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/89d61dc2a52f9413360c65d32714d577/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+  atOptions = {
+    'key' : 'f1f0daddbd8ff170fe8a239c5d534ed2',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/f1f0daddbd8ff170fe8a239c5d534ed2/invoke.js"></scr' + 'ipt>');
+</script>
+
+
+<!---ADS--->
     <div style="height: 600px ;width:400px">
 <canvas id="chart2"  style="width:50%;max-width:750px"></canvas>
 </div>
@@ -125,6 +149,7 @@ if ($result->num_rows > 0) {
   $stats = (($total*0.035)/7000)*100;
 
 ?>
+<br>
 <style>
 
       .progress-bar {
@@ -162,7 +187,6 @@ elem.textContent = "<?php echo $stats ; ?>%";
  pic.setAttribute('src', temp);
 </script>
 <?php
-
 
 
 
@@ -263,6 +287,26 @@ options: {
     maintainAspectRatio: false
 }
 });
+</script>
+<script type="text/javascript">
+  atOptions = {
+    'key' : '6201053067b6652a3e4c92c1e0145b6f',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/6201053067b6652a3e4c92c1e0145b6f/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+  atOptions = {
+    'key' : '5dfed61463bd5356ae701e3a69964dc4',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/5dfed61463bd5356ae701e3a69964dc4/invoke.js"></scr' + 'ipt>');
 </script>
 </html>
 <?php 
