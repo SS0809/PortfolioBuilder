4228b821b63d451bd5cb11531b9013a81c0a2e1b
SS0809 <saurabh45215@gmail.com>
2022-09-22 15:39:47 +0530

    Update times.php

--- view/times.php
+++ view/times.php
@@ -1,9 +1,13 @@
 <?php
 
-/*date_default_timezone_set("Asia/Calcutta");
-echo date("Y/m/d H:i:s");   */
-//error_reporting(0);
-include '../dbconnect.php';
-   $sql = "UPDATE registered_users SET points=points-2 where pay = '1' && sudo = '1';";
-   $result = mysqli_query($conn, $sql); 
+date_default_timezone_set("Asia/kolkata");
+$t = time();   
+$t1 = "1663774678";
+if ($t > $t1+ (12*60*60)) {
+   echo "a";
+}
+else
+{
+   echo "b";
+}
 ?>
