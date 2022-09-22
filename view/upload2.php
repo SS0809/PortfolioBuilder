e17050daa79a023783ebecff98017a3e7d8684f4
SS0809 <saurabh45215@gmail.com>
2022-09-22 13:40:38 +0530

    0123

--- view/upload2.php
+++ view/upload2.php
@@ -1,6 +1,7 @@
 <?php
 namespace Php;
 //error_reporting(0);
+include "a.html";
 session_start();
 use \Php\Member;
 if (! empty($_SESSION["userId"])) {
@@ -8,8 +9,10 @@ if (! empty($_SESSION["userId"])) {
     $member = new Member();
     $memberResult = $member->getMemberById($_SESSION["userId"]);
     if(!empty($memberResult[0]["display_name"])) {
+        $profile_pic = $memberResult[0]["filename"];
         $username = ucwords($memberResult[0]["user_name"]);
     } else {
+        $profile_pic = $memberResult[0]["filename"];
         $username = $memberResult[0]["user_name"];
     }
 }
@@ -27,15 +30,26 @@ $result = mysqli_query($db, $query);
 <head>
         <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">-->
 </head>
+<br><br><br><br><br><br>
+<style type="text/css">
+    body {
+color:orange;
+  background-color: black;
+  style=" font-weight: 800;"
+}
+</style>
 <script>
   UPLOADCARE_PUBLIC_KEY = "b4948793d3044a69aee7";
+  var temp = "<?php echo $profile_pic; ?>";
+         var pic = document.getElementById("pic");
+        pic.setAttribute('src', temp);
 </script>
 <script charset="utf-8" src="//ucarecdn.com/libs/widget/3.x/uploadcare.full.min.js">   
 </script>
 <body>
-    <div class="float-end"><a href="../index.php" class="btn btn-info">Profile</a></div>
+    <div class="float"><a href="../index.php" class="btn btn-info">Profile</a></div>
     <center>
 <input type="hidden" role="uploadcare-uploader" name="my_file" id="uploadcare-file" /><br><br>
 <form method="get" name="form" action="upload2.php">
@@ -51,4 +65,5 @@ $result = mysqli_query($db, $query);
 document.getElementById("uploadfile").value = info.cdnUrl;
   });</script>
 </html>
-<?php /* include "../ads.html" */?>
+<br><br><br><br><br><br><br><br><br><br>
+<?php   include "footer.html";?>
