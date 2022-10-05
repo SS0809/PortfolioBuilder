66fb9e0355fc5f1923b6f5c5924c21a44f01990a
SS0809 <saurabh45215@gmail.com>
2022-10-05 12:40:14 +0530

    switch

--- view/dashboardnew.php
+++ view/dashboardnew.php
@@ -120,7 +120,8 @@ else
     <link href="./view/carousel.css" rel="stylesheet">
   </head>
   <body> 
-<?php include_once("a.html") ?>    
+<?php include_once("a.html") ?>   
+
 <script type="text/javascript">
           var temp = "<?php echo $profile_pic; ?>";
          var pic = document.getElementById("pic");
@@ -137,6 +138,7 @@ else
   navigator.clipboard.writeText(copyText.value);
   navigator.clipboard.writeText(copyText.value);
 }</script>  
+<br><br><br><br>
 <style type="text/css">
      .iconDetails {
  margin-left:2%;
@@ -156,9 +158,133 @@ body {
    background-image: linear-gradient(120deg,  black, #1C2A3D);
   style=" font-weight: 800;"
 }
-</style>
+  /* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: grey;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .2s;
+  transition: .2s;
+}
+input:checked + .slider {
+  background-color: blueviolet;
+}
+input:focus + .slider {
+  box-shadow: 0 0 1px blueviolet;
+}
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+.slider.round:before {
+  border-radius: 50%;
+}content: "h";
+  </style>
 
-<br><br><br><br>
+<script>function putStatus() {
+    $.ajax({
+        type: "GET",
+        url: "view/iindex.php",
+        data: {toggle_select: true},
+        success: function (result) {
+            if (result == 1) {
+                $('#customSwitch1').prop('checked', true);
+                statusText(1);
+            } else {
+                $('#customSwitch1').prop('checked', false);
+                statusText(0);
+            }
+            lastUpdated();
+        }
+    });
+}
+
+function statusText(status_val) {
+    if (status_val == 1) {
+        var status_str = "On (1)";
+    } else {
+        var status_str = "Off (0)";
+    }
+    document.getElementById("statusText").innerText = status_str;
+}
+
+function onToggle() {
+    $('#toggleForm :checkbox').change(function () {
+        if (this.checked) {
+            //alert('checked');
+            updateStatus(1);
+            statusText(1);
+        } else {
+            //alert('NOT checked');
+            updateStatus(0);
+            statusText(0);
+        }
+    });
+}
+
+function updateStatus(status_val) {
+    $.ajax({
+        type: "POST",
+        url: "view/iindex.php",
+        data: {toggle_update: true, status: status_val},
+        success: function (result) {
+            console.log(result);
+            lastUpdated();
+        }
+    });
+}
+
+function lastUpdated() {
+    $.ajax({
+        type: "GET",
+        url: "view/iindex.php",
+        data: {toggle_updated: true},
+        success: function (result) {
+            document.getElementById("updatedAt").innerText = "Last updated at: " + result;
+        }
+    });
+}
+
+$(document).ready(function () {
+    //Called on page load:
+    putStatus();
+    onToggle();
+    statusText();
+});
+</script>
 <div>
 <div class="container">
   <div class="row">
@@ -215,14 +341,27 @@ $lastpoint = substr($lastpoint, 0, strpos($lastpoint, "?cc"));
  elem.style.width = "<?php if ($lastpoint!="") {echo $lastpoint*2;}else echo "50" ?>%";
 elem.textContent = "<?php if ($lastpoint!="") {echo $lastpoint*2;}else echo "ZERO POINTS COLLECTED" ?> %";
 </script>
-<br><br>
+<br><br><!--
      Enter 1 to serve your web && 0 to not 
       <form id="myForm" action="#" method="post">
     <input type="input" id="pay_o" name="pay_o" value="0 or 1">
    <input class="btn btn-danger me-1" type="submit" name="login" value="Done"
                         class="btnLogin">
-    </form>
-<a href="https://subxsub.me/pages?a=<?php echo $username; ?>" target="_blank" class="btn btn-success me-1" role="button">portfolio</a>
+    </form>-->
+    <!-- Rounded switch -->
+<form method="post" id="toggleForm">
+          <fieldset>
+<h5>PORTFOLIO AVAILABILITY </h5>
+<label class="switch">    
+<input type="checkbox" class="custom-control-input" id="customSwitch1" name='machine_state'>
+<span class="slider round">
+  </span>
+</label>
+<div class="custom-control custom-switch">
+<label class="custom-control-label" id="statusText" for="customSwitch1"></label>
+              </div>
+            </form>
+<a href="https://subxsub.me/pages?a=<?php echo $username; ?>" target="_blank" class="btn btn-success me-1" role="button">OPEN</a>
 <a href="/view/info.php" target="_blank" class="btn btn-success me-1" role="button">EDIT</a><br>
 Copy the following text and share it in your bio {your portfolio}<br>
  <input type="" value="https://subxsub.me/pages?a=<?php echo $username; ?>" id="myInput">
