ad74e97f9abe33c9b5230782d123b14e688f4a87
Saurabh Saraswat <saurabh45215@gmail.com>
2022-10-11 14:56:14 +0530

    Update login-form.php

--- view/login-form.php
+++ view/login-form.php
@@ -5,10 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="./view/css/style.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
-body
+html
+  {
+     background-image: linear-gradient(120deg,  black, #1C2A3D);
+  }
+  body
 {
      color: white;
-   background-image: linear-gradient(120deg,  black, #1C2A3D);
 }
 #super{
   animation: blink 2s ease forwards;
