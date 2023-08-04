45a637f7f00ebfe38255818cabe1c6c04e9960f6
SS08090 <techicalsolutions68@gmail.com>
2023-08-04 15:40:58 +0530

    updated to database only

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
-# Use the official PHP image as the base image
-FROM php:latest
 
-# Install any additional PHP extensions your application needs.
-# For example, if your application requires mysqli extension:
-# RUN docker-php-ext-install mysqli
+FROM mysql:latest
 
-# Copy the PHP application files into the container
-COPY app/ /var/www/html/
 
-# Set the working directory
-WORKDIR /var/www/html/
+COPY init.sql /docker-entrypoint-initdb.d/
 
-# Expose port 80 for the PHP server
-EXPOSE 80
 
-# Install and configure MySQL client inside the container
-RUN apt-get update && apt-get install -y default-mysql-client
+ENV MYSQL_ROOT_PASSWORD=0111cs211163
+ENV MYSQL_DATABASE=music23
+
+
+EXPOSE 3306
--- init.sql
+++ init.sql
@@ -1,26 +1,29 @@
 CREATE DATABASE IF NOT EXISTS music23;
-CREATE TABLE music23.registered_users (
-  id int  NOT NULL,
-  user_name varchar (100)  NOT NULL,
-  display_name varchar (100)  NOT NULL DEFAULT 'DEFAULT',
-  password varchar (100)  NOT NULL,
-  email varchar (100)  NOT NULL DEFAULT 'DEFAULT',
-  points int  NOT NULL DEFAULT 5,
-  suggest varchar (100)  DEFAULT NULL,
-  filename varchar (100)  DEFAULT NULL,
-  sudo varchar (100)  NOT NULL DEFAULT '0',
-  chart_data varchar (100)  NOT NULL DEFAULT '0, 0, 0, 0, 0, 0, 0',
-  pay int  NOT NULL DEFAULT 0,
+
+USE music23;
+
+CREATE TABLE IF NOT EXISTS registered_users (
+  id int NOT NULL,
+  user_name varchar(100) NOT NULL,
+  display_name varchar(100) NOT NULL DEFAULT 'DEFAULT',
+  password varchar(100) NOT NULL,
+  email varchar(100) NOT NULL DEFAULT 'DEFAULT',
+  points int NOT NULL DEFAULT 5,
+  suggest varchar(100) DEFAULT NULL,
+  filename varchar(100) DEFAULT NULL,
+  sudo varchar(100) NOT NULL DEFAULT '0',
+  chart_data varchar(100) NOT NULL DEFAULT '0, 0, 0, 0, 0, 0, 0',
+  pay int NOT NULL DEFAULT 0,
   goal text DEFAULT NULL,
-  sabout varchar (100)  DEFAULT NULL,
-  about varchar (100)  DEFAULT NULL,
-  youtube varchar (100)  DEFAULT NULL,
-  insta int  DEFAULT NULL,
-  user_browser varchar (400)  DEFAULT NULL,
-  pointa varchar (100)  DEFAULT NULL,
-  pointb varchar (100)  DEFAULT NULL,
-  lastpoint varchar (100)  DEFAULT NULL,
-  timestampp int  DEFAULT NULL,
-  streak int  DEFAULT 0,
-  lasttime int  DEFAULT NULL
-) 
+  sabout varchar(100) DEFAULT NULL,
+  about varchar(100) DEFAULT NULL,
+  youtube varchar(100) DEFAULT NULL,
+  insta int DEFAULT NULL,
+  user_browser varchar(400) DEFAULT NULL,
+  pointa varchar(100) DEFAULT NULL,
+  pointb varchar(100) DEFAULT NULL,
+  lastpoint varchar(100) DEFAULT NULL,
+  timestampp int DEFAULT NULL,
+  streak int DEFAULT 0,
+  lasttime int DEFAULT NULL
+);
